3de6f8528a0755614a7e5fd8565531e010647cfd
charlottesaidi <charlotte.saidi@outlook.fr>
2021-07-23 01:33:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Planets fact site solution
+
+Solution to the [Clock App challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/clock-app-LMFaxFwrM). A small clock app using World Time and Geolocation APIs. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshots](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+  - [Credits](#credits)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- View the current time and location information based on their IP address
+- View additional information about the date and time in the expanded state
+- Be shown the correct greeting and background image based on the time of day they're visiting the site
+- Generate random programming quotes by clicking the refresh icon near the quote
+
+### Screenshots
+
+- Desktop  
+![](./desktop.png)  
+- Tablet  
+![](./tablet.png)  
+- Mobile  
+![](./mobile.png)
+
+### Links
+
+- [Solution URL]()  
+- [Live Site URL]() 
+
+## My process
+
+### Built with
+
+- HTMLS
+- Sass
+- Flexbox
+- JavaScript vanilla
+- [Figma](https://www.figma.com/) and Font Installer
+
+### Useful resources
+
+- [StackOverflow](https://stackoverflow.com/)
+- [CSS-Tricks](https://css-tricks.com/)
---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+    window.onload = function() {
+    displayRandomQuotes();
+    dayOrNight();
+    getLocation();
+}
+
+// FUNCTIONS
+function displayRandomQuotes() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload  = function() {
+        const quote = this.response.content;
+        const author = this.response.author;
+        document.getElementById("quote").innerHTML = quote;
+        document.getElementById("author").innerHTML = author;
+    }
+    xhttp.responseType = "json";
+    xhttp.open("GET", "https://api.quotable.io/random", true);
+    xhttp.send();
+} 
+
+function dayOrNight() {
+    const xhttp = new XMLHttpRequest();
+        xhttp.onload  = function() {
+            const time = new Date(this.response.datetime);
+            const timezone = this.response.abbreviation;
+            const currTimezone = this.response.timezone;
+            const yearDay = this.response.day_of_year;
+            const weekDay = this.response.day_of_week;
+            const weekNumber = this.response.week_number;
+
+            showTime();
+
+            // document.getElementById("time").innerHTML = time.format("HH:MM");
+            document.getElementById("timezone").innerHTML = timezone;
+            document.getElementById("curr_timezone").innerHTML = currTimezone;
+            document.getElementById("year_day").innerHTML = yearDay;
+            document.getElementById("week_day").innerHTML = weekDay;
+            document.getElementById("week_number").innerHTML = weekNumber;
+
+            if(time.getHours() >= 5 && time.getHours() <= 12 ) {
+                document.getElementById("greet").innerHTML = "Good morning";
+            } else if(time.getHours() >= 12 && time.getHours() <= 18 ) {
+                document.getElementById("greet").innerHTML = "Good afternoon";
+            } else {
+                document.getElementById("greet").innerHTML = "Good evening";
+            }
+
+            var greetingIcon = document.getElementById("greeting_icon");
+            var backgroundFilterBlock = document.getElementById("background-filter");
+            var hiddenStatSection = document.getElementById("hidden_section");
+            var firstStatBlockBorder = document.getElementById("first_stats_block");
+
+            if(time.getHours() >= 5 && time.getHours() <= 12 ) {
+                document.body.classList.add('daytime_background');
+                greetingIcon.src = 'assets/images/desktop/icon-sun.svg';
+                backgroundFilterBlock.classList.add('background-filter');
+                hiddenStatSection.classList.add('light_background');
+                hiddenStatSection.classList.add('dark_police');
+                firstStatBlockBorder.classList.add('dark_border');
+            } else {
+                document.body.classList.add('nightime_background');
+                greetingIcon.src = 'assets/images/desktop/icon-moon.svg';
+                hiddenStatSection.classList.add('dark_background');
+                firstStatBlockBorder.classList.add('light_border');
+            }
+        }
+    xhttp.responseType = "json";
+    xhttp.open("GET", "http://worldtimeapi.org/api/ip", true);
+    xhttp.send();
+}
+
+function getLocation() {
+    const xhttp = new XMLHttpRequest();
+        xhttp.onload  = function() {
+            const town = this.response.city;
+            const country = this.response.country_code;
+            document.getElementById("town").innerHTML = town;
+            document.getElementById("country").innerHTML = country;
+        }
+    xhttp.responseType = "json";
+    xhttp.open("GET", "https://freegeoip.app/json/", true);
+    xhttp.send();
+}
+
+function showTime() {
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+    var currentTimeString = currentHours + ":" + currentMinutes;
+    document.getElementById("time").innerHTML = currentTimeString;
+    setInterval(showTime, 1000);
+}
+
+function showOrHide() {
+    var hiddenStatSection = document.getElementById('hidden_section');
+    var randomQuoteSection = document.getElementById('quote_section');
+    var dateTimeSection = document.getElementById('time_section');
+    var moreLessBtn = document.getElementById('more_less');
+    var btnArrow = document.getElementById('arrow');
+
+    if(hiddenStatSection.classList.contains("hidden")) {
+        hiddenStatSection.classList.replace("hidden", "show_section");
+    } else {
+        hiddenStatSection.classList.replace("show_section", "hidden");
+    }
+
+    if(randomQuoteSection.classList.contains("random_quotes")) {
+        randomQuoteSection.classList.replace('random_quotes', "hidden");
+    } else {
+        randomQuoteSection.classList.replace( "hidden", 'random_quotes');
+    }
+    
+    if(dateTimeSection.classList.contains('toggle_padding')) {
+        dateTimeSection.classList.remove('toggle_padding');
+    } else {
+        dateTimeSection.classList.add('toggle_padding');
+    }
+    
+    if(btnArrow.classList.contains('arrow_down')) {
+        btnArrow.classList.replace('arrow_down', "arrow_up")
+    } else {
+        btnArrow.classList.replace("arrow_up", 'arrow_down')
+    }
+
+    if(moreLessBtn.innerHTML == "More") {
+        moreLessBtn.innerHTML = "Less"
+    } else {
+        moreLessBtn.innerHTML = "More"
+    }
+}
